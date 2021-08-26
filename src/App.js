1816c493dbc66e775c29f48861b38cc453ffd33b
Bar <51476543+bardabun@users.noreply.github.com>
2021-08-26 11:04:45 +0300

    form frontend

--- src/App.js
+++ src/App.js
@@ -5,15 +5,68 @@ import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Services from './components/pages/Services';
 import Products from './components/pages/Products';
-import SignUp from './components/pages/SignUp';
+import SignUp from './components/pages/signup/SignUp';
+import api from './api/posts'
+import { useState, useEffect } from 'react';
 
 function App() {
+const [posts, setPosts] = useState([
+  {
+    "posts": [
+      {
+        "id": 1,
+        "title": "1st post",
+        "datetime": "July 16, 2021 11:47:39 AM",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      },
+      {
+        "id": 2,
+        "title": "Second post",
+        "datetime": "July 16, 2021 11:47:48 AM",
+        "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. two"
+      },
+      {
+        "id": 3,
+        "title": "Number Three",
+        "datetime": "July 16, 2021 11:48:01 AM",
+        "body": "Third post... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      },
+      {
+        "id": 4,
+        "title": "Testing a 4th post",
+        "datetime": "August 02, 2021 11:46:27 AM",
+        "body": "Some more testing paragraphs!"
+      }
+    ]
+  }
+])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (err) {
+        if (err.response) {
+          // Not in the 200 response range 
+          console.log(err.response.data);
+          console.log(err.response.status);
+          console.log(err.response.headers);
+        } else {
+          console.log(`Error: ${err.message}`);
+        }
+      }
+    }
+
+    fetchPosts();
+  }, [])
+
   return (
     <>
     <Router>
       <Navbar />
       <Switch>
-          <Route path='/' exact component={Home} />
+          <Route path='/' exact component={Home}/>
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
